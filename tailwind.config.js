de4171374887437c7a9b622cd9bac3e83acbbeb8
Aszaychik <aszaychik@gmail.com>
2023-03-26 19:50:15 +0700

    tailwinds setting

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#F9F5EB',
+        'color-primary': '#002B5B',
+        'color-secondary': '#EA5455',
+      },
+    },
+  },
+  plugins: [],
+}
